09f347a0c4273d2f51cc356835b410918122cce2
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2023-01-17 08:06:29 +0700

    feat: decrypt pembelian data in prosescetak.php

--- prosescetak.php
+++ prosescetak.php
@@ -2,11 +2,13 @@
 session_start();
 error_reporting(0);
 header("Content-type: application/force-download");
-header("Content-Disposition: attachment; filename=EDEL_notapembelian_".$_GET[id].".xls");
+header("Content-Disposition: attachment; filename=EDEL_notapembelian_" . $_GET[id] . ".xls");
 ?>
 
-<center><h2>Nota Pembelian #<?php echo $_GET['id']?></h2></center>
-    
+<center>
+  <h2>Nota Pembelian #<?php echo $_GET['id'] ?></h2>
+</center>
+
 <table border="1" cellpadding="5">
   <tr>
     <th>No</th>
@@ -17,41 +19,68 @@ header("Content-Disposition: attachment; filename=EDEL_notapembelian_".$_GET[id]
     <th>Subtotal</th>
   </tr>
   <?php
-  
+
   include "koneksi.php";
-  
-  
+  include '../des/encrypt.php';
+
+
   $query = $conn->query("SELECT * FROM pembelian_produk JOIN produk 
     ON pembelian_produk.id_produk=produk.id_produk JOIN warung ON produk.id_warung=warung.id_warung
     WHERE pembelian_produk.id_pembelian='$_GET[id]'");
 
-  $no = 1; 
-  while($data = $query->fetch_assoc()){ 
-    ?>
+  $no = 1;
+  while ($data = $query->fetch_assoc()) {
+  ?>
     <tr>
       <td><?php echo $no++; ?></td>
       <td><?php echo $data['nama_produk']; ?></td>
       <td><?php echo $data['nama_warung']; ?> (<?php echo $data['telepon_warung']; ?>)</td>
       <td>Rp.<?php echo number_format($data['harga_produk']); ?></td>
       <td><?php echo $data['jumlah'] ?></td>
-      <td>Rp.<?php echo number_format($data['harga_produk']*$data['jumlah']); ?></td>
+      <td>Rp.<?php echo number_format($data['harga_produk'] * $data['jumlah']); ?></td>
     </tr>
-    <?php
+  <?php
   }
-  $query2=$conn->query("SELECT * FROM pembelian WHERE id_pembelian=$_GET[id]");
-  $data2=$query2->fetch_assoc();
+  $query2 = $conn->query("SELECT * FROM pembelian WHERE id_pembelian=$_GET[id]");
+  $data2 = $query2->fetch_assoc();
+
+  $encrypt_result = [
+    'tanggal' => trim($data2['tanggal_pembelian']),
+    'total' => trim($data2['jumlah_pembelian']),
+    'ongkir' => trim($data2['ongkir']),
+    'bayar' => trim($data2['total_pembelian'])
+  ];
+
+  $decrypt_result = array();
+
+  $desModule = new Encrypt();
+
+  foreach ($encrypt_result as $key => $value) {
+    $plaintext = "";
+    $ciphertext = "";
+
+    $arr_ciphertext = str_split($value, 64);
+
+    foreach ($arr_ciphertext as $i) {
+      $decrypt = $desModule->decrypt($i, '12345678');
+      $plaintext .= $desModule->read_bin($decrypt);
+      $ciphertext .= $desModule->read_bin($i);
+    }
+
+    $decrypt_result[$key] = $plaintext;
+  }
   ?>
   <tr>
     <th colspan="5" class="text-right">Total Pembelian</th>
-    <th>Rp.<?php echo number_format($data2['jumlah_pembelian']); ?></th>
+    <th>Rp.<?php echo number_format(intval($decrypt_result['total'])); ?></th>
   </tr>
   <tr>
     <th colspan="5" class="text-right">Ongkos Kirim</th>
-    <th>Rp.<?php echo number_format($data2['ongkir']); ?></th>
+    <th>Rp.<?php echo number_format(intval($decrypt_result['ongkir'])); ?></th>
   </tr>
   <tr>
     <th colspan="5" class="text-right"><b>Total</b></th>
-    <th><b>Rp.<?php echo number_format($data2['total_pembelian']); ?></b></th>
+    <th><b>Rp.<?php echo number_format(intval($decrypt_result['bayar'])); ?></b></th>
   </tr>
   <?php
   ?>
