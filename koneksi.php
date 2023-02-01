01a34bdbd2e72edb9e8e93f759e5f1af5daf4b9e
umarhadi <hi@umarhadi.dev>
2023-02-01 22:03:52 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,3 @@
 <?php 
-	$conn= new mysqli('localhost','root','','database_cikwo');
+	$conn= new mysqli('very-mysql.c1kuewzcqwcc.ap-southeast-3.rds.amazonaws.com:6969','umar','bebekbalap','cikwo');
 ?>
