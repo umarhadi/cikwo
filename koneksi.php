46117edc78f05ef56baf940568586a35fda3f0ce
umarhadi <hi@umarhadi.dev>
2023-02-02 21:18:36 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,3 @@
 <?php 
-	$conn= new mysqli('very-mysql.c1kuewzcqwcc.ap-southeast-3.rds.amazonaws.com:6969','umar','bebekbalap','cikwo');
+	$conn= new mysqli('storage.wkwkwk.fun:7070','root','bebekbalap','cikwo');
 ?>
